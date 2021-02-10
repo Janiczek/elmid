0bf0d8c57390dd7a57482bb02a0bcd4898e37344
Martin Janiczek <martin@janiczek.cz>
2021-02-10 08:59:28 +0100

    Expando works, elm-make doesn't yet

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,46 @@
 module Main where
 
 import Brick
+import Brick.BChan
+import Cherry.Prelude
+import Control.Concurrent (forkIO)
+import Control.Monad (void, forever)
 import Lib
-import Prelude (IO)
+import Prelude (IO, (=<<), return, Maybe(Nothing), FilePath)
+import Debug
+import String
+import System.Process (readProcess)
+import Twitch ((|+),(|%),(|-))
+import qualified Graphics.Vty as V
+import qualified Twitch as T
 
 main :: IO ()
-main = simpleMain ui
+main = do
+  chan <- newBChan 10
+  void <| forkIO <| forever <| watchElmFiles chan
+
+  let buildVty = do
+        v <- V.mkVty =<< V.standardIOConfig
+        V.setMode (V.outputIface v) V.Mouse True
+        return v
+
+  initialVty <- buildVty
+
+  void <| customMain initialVty buildVty Prelude.Nothing app initModel
+
+watchElmFiles :: BChan String -> IO ()
+watchElmFiles chan =
+  T.defaultMain <| do
+    -- TODO this *.elm path configurable
+    "client/*.elm" |+ runElmMake -- on add
+                   |% runElmMake -- on modify
+                   |- runElmMake -- on delete
+      where
+        runElmMake :: FilePath -> IO ()
+        runElmMake _ = do
+          -- TODO make path to Elm configurable
+          -- TODO make path to Main.elm configurable
+          stdout <- readProcess "elm" ["make", "client/app-monolithic/src/Entry.elm", "--output", "/dev/null"] ""
+          writeBChan chan (String.fromList stdout)
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,31 +1,133 @@
-module Lib (ui) where
+module Lib (initModel, app) where
 
 import Brick
+import Brick.AttrMap
+import qualified Brick.Types as T
+import qualified Debug
 import Cherry.Prelude
-import String
 import List
+import String
+import qualified Graphics.Vty as V
+import Prelude (return, show, Show, Ord, Eq, error)
 
-data CompilationError
-  = TodoAddSomeMoreDetails Int
+data Model = Model
+  { mErrors :: List Error
+  }
 
-compilationErrors :: List CompilationError
-compilationErrors = 
-  [ TodoAddSomeMoreDetails 42
-  , TodoAddSomeMoreDetails (-1)
-  ]
+data Error = Error
+  { eData :: ErrorData
+  , eExpanded :: Bool
+  }
 
-ui :: Widget ()
-ui = 
-  if List.isEmpty compilationErrors then
-    goodUi
+data ErrorData
+  = DummyError Int
+
+initModel :: Model
+initModel =
+  Model { mErrors = errors }
+    where
+      errors =
+        [ Error 
+          { eData = DummyError 42
+          , eExpanded = True 
+          }
+        , Error 
+          { eData = DummyError (-1)
+          , eExpanded = False 
+          }
+        , Error 
+          { eData = DummyError 999
+          , eExpanded = True
+          }
+        ]
+
+data Name = ErrorAtIndex Int 
+  deriving (Show, Ord, Eq)
+
+app :: App Model () Name
+app =
+  App
+    { appDraw = draw
+    , appChooseCursor = showFirstCursor
+    , appHandleEvent = handleEvent
+    , appStartEvent = return
+    , appAttrMap = attributeMap
+    }
+
+------ DRAW
+
+draw :: Model -> List (Widget Name)
+draw model = 
+  let
+    errors = mErrors model
+  in
+  if List.isEmpty errors then
+    [drawGoodUi]
   else
-    badUi
+    [drawBadUi errors]
 
-goodUi :: Widget ()
-goodUi = str "All good!"
+drawGoodUi :: Widget Name
+drawGoodUi = str "All good!"
 
-badUi :: Widget ()
-badUi = 
-  String.fromInt (List.length compilationErrors) ++ " errors :("
-    |> String.toList
-    |> str
+drawBadUi :: List Error -> Widget Name
+drawBadUi errors = 
+  errors
+    |> List.indexedMap drawError
+    |> vBox
+
+drawError :: Int -> Error -> Widget Name
+drawError i err =
+  if eExpanded err then
+    drawExpandedError i <| eData err
+  else
+    drawCollapsedError i <| eData err
+
+drawExpandedError :: Int -> ErrorData -> Widget Name
+drawExpandedError i (DummyError n) =
+  hBox
+    [ clickable (ErrorAtIndex i) <| str "[-] "
+    , vBox
+        [ clickable (ErrorAtIndex i) <| str "Dummy Error"
+        , clickable (ErrorAtIndex i) <| str "==========="
+        , str <| String.toList <| "  " ++ String.fromInt n
+        , clickable (ErrorAtIndex i) <| str " "
+        ]
+    ]
+
+
+drawCollapsedError :: Int -> ErrorData -> Widget Name
+drawCollapsedError i (DummyError n) =
+  clickable (ErrorAtIndex i) <|
+    hBox
+      [ str "[+] "
+      , str <| String.toList <| "Dummy Error " ++ String.fromInt n
+      ]
+
+------- ATTR MAP
+
+attributeMap :: Model -> AttrMap
+attributeMap _ = attrMap V.defAttr []
+
+------- EVENT
+
+handleEvent :: Model -> BrickEvent Name () -> EventM Name (Next Model)
+
+handleEvent model (T.MouseDown (ErrorAtIndex toggledIndex) _ _ _) = 
+  continue (model { mErrors = 
+    mErrors model
+    |> List.indexedMap (\i err -> 
+      if i == toggledIndex then
+        err { eExpanded = not <| eExpanded err } 
+      else
+        err
+    )})
+
+
+handleEvent model (T.VtyEvent e) = 
+  case e of
+    V.EvKey V.KEsc [] -> halt model
+    V.EvKey (V.KChar 'q') [] -> halt model
+    V.EvKey (V.KChar 'c') [V.MCtrl] -> halt model
+    _ -> case Debug.log "vty event" e of _ -> continue model
+
+handleEvent model _ = continue model
