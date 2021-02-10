7a08a4eaaeeb02d715680f178048d75c7988e4c5
Martin Janiczek <martin@janiczek.cz>
2021-02-10 22:23:46 +0100

    use linux-inotify instead

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,16 @@ module Main where
 import Brick
 import Brick.BChan
 import Cherry.Prelude
-import Control.Concurrent (forkIO)
-import Control.Monad (void, forever)
+import Control.Concurrent (forkIO, threadDelay)
+import Data.Text as T
+import Data.Text.Encoding as TE
+import Control.Monad (void, forever, when)
 import Debug
 import System.FilePath.Glob (Pattern)
 import Lib
-import Prelude (IO, (=<<), return, Maybe(..), FilePath, putStrLn, error)
+import Prelude (IO, String, (=<<), return, Maybe(..), FilePath, putStrLn, error)
 import String
-import System.FSNotify as FS
+import System.Linux.Inotify as FS
 import System.Process (readProcessWithExitCode)
 import qualified System.FilePath.Glob as Glob
 import qualified Graphics.Vty as V
@@ -29,32 +31,33 @@ main = do
 
   void <| customMain initialVty buildVty (Prelude.Just chan) app initModel
 
-watchElmFiles :: BChan Msg -> IO ()
-watchElmFiles chan = do
-  mgr <- FS.startManager
-  -- TODO make the path to file-watch configurable
-  void <| FS.watchTree mgr "client" shouldRecompile recompile
-  where
-    shouldRecompile :: Event -> Bool
-    shouldRecompile event =
-      isElmFile <| eventPath event
-
-    isElmFile :: FilePath -> Bool
-    isElmFile path =
-      Glob.match elmFilePattern path
-
-    recompile :: Event -> IO ()
-    recompile event = do
-      -- TODO make path to Elm configurable
-      -- TODO make path to Main.elm configurable
-      writeBChan chan <| Log <| "running elm make because of " ++ String.fromList (eventPath event)
-      (exitCode, stdout, stderr) <- readProcessWithExitCode "elm" ["make", "client/app-monolithic/src/Entry.elm", "--output", "/dev/null"] ""
-      writeBChan chan <| GotElmMakeOutput 
-        ( exitCode
-        , String.fromList stdout
-        , String.fromList stderr
-        )
+isElmFile :: FilePath -> Bool
+isElmFile path =
+  Glob.match elmFilePattern path
 
 elmFilePattern :: Pattern
 elmFilePattern =
   Glob.compile "**/*.elm"
+
+watchElmFiles :: BChan Msg -> IO ()
+watchElmFiles chan = do
+  inotify <- FS.init
+  -- TODO make the path to file-watch configurable
+  FS.addWatch inotify "client" FS.in_CLOSE_WRITE
+  forever <| do
+    event <- FS.getEvent inotify
+    let path = T.unpack <| TE.decodeUtf8 <| FS.name event
+    when (isElmFile path) <|
+      recompile path chan
+
+recompile :: Prelude.String -> BChan Msg -> IO ()
+recompile path chan = do
+  -- TODO make path to Elm configurable
+  -- TODO make path to Main.elm configurable
+  writeBChan chan <| Log <| "running elm make because of " ++ String.fromList path
+  (exitCode, stdout, stderr) <- readProcessWithExitCode "elm" ["make", "client/app-monolithic/src/Entry.elm", "--output", "/dev/null"] ""
+  writeBChan chan <| GotElmMakeOutput 
+    ( exitCode
+    , String.fromList stdout
+    , String.fromList stderr
+    )
